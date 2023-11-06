228f6b9bd4d3d6a1a081e68d5d7df2045503049d
hologutiBiz <hologutibusinesscentre@gmail.com>
2023-11-06 04:49:29 +0100

    vlb for lunch

---
+++ script.js
@@ -0,0 +1,32 @@
+
+    // dialog //
+// let dialog = document.querySelector('dialog');
+// document.querySelector('#show').onclick = function() {
+//     dialog.show();
+// };
+// document.querySelector('#close').onclick = function() {
+//     dialog.close();
+//     document.getElementById('dis').style.display = 'none';
+// };
+
+
+
+    // page orientation
+let pageOrt = document.getElementById('pageOrt');
+let container = document.getElementById('iframeContainer');
+
+pageOrt.addEventListener("click", function() {
+    if(container.style.flexWrap === 'wrap') {
+        container.style.flexWrap = 'nowrap';
+        // container.style.justifyContent = 'center';
+        pageOrt.innerText = "change to potrait";
+
+    } else {
+        container.style.flexWrap = 'wrap';
+        // container.style.justifyContent = 'start';
+        pageOrt.innerText = "Change to Landscape";
+    }
+});
+
+
+   
