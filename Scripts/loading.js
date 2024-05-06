b2dd0b87eba93c18bfee5f77737dc845e287fb8a
hologutiBiz <hologutibusinesscentre@gmail.com>
2024-05-06 16:55:00 +0100

    recommit

--- Scripts/loading.js
+++ Scripts/loading.js
@@ -1,4 +1,10 @@
 
+let loadingTime = 3000;
+    document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('loading').style.display = 'block';
+});
+
+
 window.addEventListener('load', function () {
     document.getElementById('loading').style.display = 'none';
 });
