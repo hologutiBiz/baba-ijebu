f7cc5b797ecf1e3663cac9661d0dd2d3b16264cf
hologutiBiz <hologutibusinesscentre@gmail.com>
2024-05-13 00:06:50 +0100

    MAY 12/Sunday/2024

--- Scripts/carry1st.js
+++ Scripts/carry1st.js
@@ -1,18 +1,18 @@
 
-const images = document.querySelectorAll('.product-images img');
-    let index = 0;
+// const images = document.querySelectorAll('.product-images img');
+//     let index = 0;
 
-    function fadeInNextImage() {
-        const currentImage = images[index];
-        const nextIndex = (index + 1) % images.length;
-        const nextImage = images[nextIndex];
+//     function fadeInNextImage() {
+//         const currentImage = images[index];
+//         const nextIndex = (index + 1) % images.length;
+//         const nextImage = images[nextIndex];
 
-        currentImage.style.opacity = '0';
-        nextImage.style.opacity = '1';
+//         currentImage.style.opacity = '0';
+//         nextImage.style.opacity = '1';
 
-        index = nextIndex;
+//         index = nextIndex;
 
-        setTimeout(fadeInNextImage, 4000); // Adjust timing as needed
-    }
+//         setTimeout(fadeInNextImage, 4000); // Adjust timing as needed
+//     }
 
-    fadeInNextImage();
+//     fadeInNextImage();
